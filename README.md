8f54a43a56788a5c775c9afd5c690c9dabe71508
Jarvist Moore Frost <jarvist@fakemail.fake>
2014-01-16 15:45:34 +0000

    Initial commit. No code, just the Mersenne Twister.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Starry Night
+
+Monte Carlo codes to simulate the ferroelectric domains in a hybrid perovskite
+solar cell.
+
+
---
+++ starrynight.c
@@ -0,0 +1,19 @@
+/* Starry Night - a Monte Carlo code to simulate ferroelectric domain formation
+ * and behaviour in hybrid perovskite solar cells.
+ *
+ * By Jarvist Moore Frost
+ * University of Bath
+ *
+ * File begun 16th January 2014
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include "mt19937ar-cok.c"
+
+int main(void)
+{
+    fprintf(stderr,"Starry Night - Monte Carlo brushstrokes.\n");
+
+    return 0;
+}
