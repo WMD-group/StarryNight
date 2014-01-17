43794dc4e29fb68fc07f21231ca383aa3756a605
Jarvist Moore Frost <jarvist@fakemail.fake>
2014-01-17 02:17:57 +0000

    Fix to the movie script.

--- make_movie.sh
+++ make_movie.sh
@@ -1,6 +1,8 @@
-for file in MC_step*.pnm
+for file in MC-PNG_step*.png
 do
+    echo -n "."
  convert -scale 500x500 "${file}" "${file%.*}.jpg" #Nb: scale = no interpolation between pixels
 done
 
-mencoder -ovc lavc mf://MC_step*.jpg -o movie.avi
+mplayer mf://MC-PNG_step*.jpg
+#mencoder -ovc lavc mf://MC-PNG_step*.jpg -o movie.avi
