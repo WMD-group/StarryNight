5cb0457c2e9c309a8a1cabc76f3e9f3e029be669
Jarvist Moore Frost <jarvist@gmail.com>
2014-04-30 14:10:00 +0100

    Fixed up export_svg to work with Inkscape on Mac OSX (painful!)

--- export_svg.sh
+++ export_svg.sh
@@ -1,12 +1,19 @@
 echo "WARNING: hard coded to size of lattice!"
 
+# Sensible Linux
+INKSCAPE="inkscape"
+# MAC OS X
+INKSCAPE="/Applications/Inkscape.app/Contents/Resources/script"
+PWD=` pwd `
+
+
 LATTICE=50
 PIXELS=500
 
 for i 
 do
- inkscape --export-area=0:0:${LATTICE}:${LATTICE}    \
- --export-png=${i%.*}.png  \
+ ${INKSCAPE} --export-area=0:0:${LATTICE}:${LATTICE}    \
+ --export-png=${PWD}/${i%.*}.png  \
  --export-width=${PIXELS}                \
- --export-height=${PIXELS} ${i} 
+ --export-height=${PIXELS} ${PWD}/${i} 
 done
