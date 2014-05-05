936e00d2c23a01588b19584e445b9e2c281c1ae0
Jarvist Moore Frost <jarvist@gmail.com>
2014-05-05 20:16:06 +0100

    Code to deal with T-dep Dipole SVG/PNG files.

---
+++ coalesce_dipoles.sh
@@ -0,0 +1,11 @@
+# coalesce / merge Inkscape SVG renders + PNG checkerboards into a super duper visual
+
+for i in Dipole_pot_xy_T*MC-SVG_final.png
+do
+
+ convert -scale 200x200 -coalesce "${i/SVG/PNG}" "${i}" out.png
+ mv out-1.png "${i%.png}_COMBINED.png"
+ echo -n "."
+done
+
+mplayer mf://*COMBINED.png -fps 10
