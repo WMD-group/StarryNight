WMD-group/StarryNight

commit fd7aebc3d20f1efdc39bff8ef87366036bc7fe3d

Full agreement between axes for fourier_transform & SVG & PNG. Care!

starrynight.c
@@ -16,8 +16,8 @@
 
 #include "mt19937ar-cok.c" //Code _included_ to allow more global optimisation
 
-#define X 10  // Malloc is for losers.
-#define Y 10 
+#define X 25  // Malloc is for losers.
+#define Y 25 
 
 struct dipole
 {
@@ -260,8 +260,8 @@ int main(int argc, char *argv[])
     P*=Dipole;
     // See 6.5 (p 167) in Zangwill Modern Electrodynamics
 
-    fprintf(stderr,"NORK! T: %d E: %f P: %f\n",T,Efield.x,P);
-    printf("T: %d Dipole: %f E: %f P: %f\n",T,Dipole,Efield.x,P);
+    fprintf(stderr,"NORK! T: %d E: %f P: %f polarisation: %f\n",T,Efield.x,P,polarisation());
+    printf("T: %d Dipole: %f E: %f P: %f polarisation: %f\n",T,Dipole,Efield.x,P,polarisation());
     } 
     // OK; we're finished...
 
@@ -743,10 +743,10 @@ void outputlattice_svg(char * filename)
      for (i=0;i<X;i++)
         for (k=0;k<Y;k++)
             fprintf(fo," <line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" style=\"stroke:rgb(%d,%d,%d);stroke-width:0.17\" marker-end=\"url(#triangle)\" />\n",
-                    i+0.5 - 0.4*lattice[k][i].x, 
-                    k+0.5 - 0.4*lattice[k][i].y,
-                    i+0.5 + 0.4*lattice[k][i].x,
-                    k+0.5 + 0.4*lattice[k][i].y,
+                    i+0.5 + 0.4*lattice[k][i].y, 
+                    k+0.5 + 0.4*lattice[k][i].x,
+                    i+0.5 - 0.4*lattice[k][i].y,
+                    k+0.5 - 0.4*lattice[k][i].x,
                     (int)((-lattice[k][i].z+1.0)*127.0),
                     (int)((-lattice[k][i].z+1.0)*127.0),
                     (int)((-lattice[k][i].z+1.0)*127.0)

amazing_arrows_dipole_potential.sh
@@ -1,0 +1,10 @@
+./starrynight
+. export_svg.sh MC-SVG_final.svg
+python fourier_transform_potential.py
+convert -scale 1000x1000 -coalesce fourier_transform_potential_data.png MC-SVG_final.png out.png
+mv out-1.png amazing_arrows_dipole_potential.png
+
+open amazing_arrows_dipole_potential.png
+. coalesce_this.sh MC-SVG_final.png
+open MC-SVG_final_COMBINED.png
+

coalesce_this.sh
@@ -3,9 +3,9 @@
 for i 
 do
 
- convert -scale 500x500 -coalesce "${i/SVG/PNG}" "${i}" out.png
+ convert -scale 1000x1000 -coalesce "${i/SVG/PNG}" "${i}" out.png
  mv out-1.png "${i%.png}_COMBINED.png"
  echo -n "."
 done
 
-mplayer mf://*COMBINED.png -fps 10
+#mplayer mf://*COMBINED.png -fps 10

export_svg.sh
@@ -10,8 +10,8 @@ fi
 
 PWD=` pwd `
 
-LATTICE=10
-PIXELS=500
+LATTICE=25
+PIXELS=1000
 
 for i 
 do

fourier_transform_potential.py
@@ -11,15 +11,25 @@ ax=fig.add_subplot(111)
 #data = numpy.genfromtxt("initial_pot_xy.dat") # Typically randomly oriented dipoles - useful to check power spectrum / code
 data = numpy.genfromtxt("final_pot_xy.dat")
 
-nrows, ncols = 100,100
+nrows, ncols = 25,25
 grid=data[:,2].reshape((nrows,ncols))
+
+# 6 FOOT HIGH NOTE:
+#   The AXES are screwy. Y is reversed; but this is also the case for the PNG output + the SVG output (reversed at the generation stage).
+#   Presently everything agrees. BE CAREFUL!
+
 print data
 
 plt.imshow(grid,extent=(data[:,0].min(), data[:,0].max(), data[:,1].max(), data[:,1].min()),
                    interpolation='nearest', cmap=plt.cm.RdBu) #RdBu) #, cmap=cm.gist_rainbow)
 #plt.colorbar()
-plt.tight_layout(pad=0.3) #, w_pad=0.5, h_pad=1.0) # Magic incantation for non-terrible plots
+#plt.tight_layout(pad=0.3) #, w_pad=0.5, h_pad=1.0) # Magic incantation for non-terrible plots
+
+# Make plot with no border for overlaying with direct outputs...
 plt.axis('off')
+plt.gca().set_frame_on(False)
+plt.gca().set_xticks([])
+plt.gca().set_yticks([])
 
 plt.show()
 fig.savefig("fourier_transform_potential_data.png",bbox_inches='tight', pad_inches=0)

starrynight.cfg
@@ -10,7 +10,7 @@ T: 300 #Integar value, Kelvin
 Efield: 
 {
  #x= 0.02 # Calculated value for 10M V/m (1V over 100nm) applied to 2.29 Debye dipole
- #x = 1.0
+ #x = 10.0
  x = 0.0
  y= 0.0
  z= 0.0

commit 1716acf1e75ea55013d1bbbec362476e209dbfe2

Fix to SVG output (actually undoes bug introduced in fd7aebc3d2 )

starrynight.c
@@ -744,10 +744,10 @@ void outputlattice_svg(char * filename)
      for (i=0;i<X;i++)
         for (k=0;k<Y;k++)
             fprintf(fo," <line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" style=\"stroke:rgb(%d,%d,%d);stroke-width:0.17\" marker-end=\"url(#triangle)\" />\n",
-                    i+0.5 + 0.4*lattice[k][i].y, 
-                    k+0.5 + 0.4*lattice[k][i].x,
-                    i+0.5 - 0.4*lattice[k][i].y,
-                    k+0.5 - 0.4*lattice[k][i].x,
+                    i+0.5 - 0.4*lattice[k][i].x, 
+                    k+0.5 - 0.4*lattice[k][i].y,
+                    i+0.5 + 0.4*lattice[k][i].x,
+                    k+0.5 + 0.4*lattice[k][i].y,
                     (int)((-lattice[k][i].z+1.0)*127.0),
                     (int)((-lattice[k][i].z+1.0)*127.0),
                     (int)((-lattice[k][i].z+1.0)*127.0)