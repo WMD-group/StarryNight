9ee15327af7d1329380daa408877cce05ec0fd35
Jarvist Moore Frost <jarvist@gmail.com>
2015-06-25 13:59:02 +0100

    Boolean flags in .config file to chose what analysis / output Slowly dragging the software architecture into the 1990s!

--- starrynight-config.c
+++ starrynight-config.c
@@ -7,6 +7,8 @@
  * File begun 16th January 2014
  */
 
+#include <stdbool.h>
+
 #define X 24 // Malloc is for losers.
 #define Y 24 
 #define Z 80 
@@ -14,6 +16,10 @@
 int DIM=3; //currently just whether the dipoles can point in Z-axis (still a 2D slab) 
 int T; //global variable so accessible to analysis routines
 
+unsigned long ACCEPT=0; //counters for MC moves
+unsigned long REJECT=0;
+
+// CUSTOM STRUCTURES
 struct dipole
 {
     float x,y,z;
@@ -27,36 +33,6 @@ struct mixture
 } dipoles[10];
 int dipolecount=0;
 
-// SIMULATION PARAMETERS
-// NB: These are defaults - most are now read from config file
-
-double beta=1.0;  // beta=1/T  T=temperature of the lattice, in units of k_B
-
-struct dipole Efield; //now a vector, still k_B.T units per lattice unit
-//double Efield=0.01; // units k_B.T per lattice unit
-double Eangle=0.0;
-
-double K=1.0; //elastic coupling constant for dipole moving within cage
-
-double Dipole=1.0; //units of k_B.T for spacing = 1 lattice unit
-double CageStrain=1.0; // as above
-
-double dipole_fraction=0.9; //fraction of sites to be occupied by dipoles
-
-int DipoleCutOff=3;
-
-// These variables from the old main
-int MCMegaSteps=400;
-int MCEqmSteps=10;
-double MCMegaMultiplier=1.0;
-long long int MCMinorSteps=0;
-char const *LOGFILE = NULL; //for output filenames
-//END OF SIMULATION PARAMETERS
-
-// {{ Except for the ones hardcoded into the algorithm :^) }}
-
-unsigned long ACCEPT=0; //counters for MC moves
-unsigned long REJECT=0;
 
 // Prototypes...
 static float dot(struct dipole *a, struct dipole *b);
@@ -69,8 +45,127 @@ void initialise_lattice_spectrum();
 void initialise_lattice_buckled();
 void initialise_lattice_slab_delete();
 
+// SIMULATION PARAMETERS
+// NB: These are defaults - most are now read from config file
+
+double beta=1.0;  // beta=1/T  T=temperature of the lattice, in units of k_B
+
+struct dipole Efield; //now a vector, still k_B.T units per lattice unit
+
+double K=1.0; //elastic coupling constant for dipole moving within cage
+double Dipole=1.0; //units of k_B.T for spacing = 1 lattice unit
+double CageStrain=1.0; // as above
+
+double dipole_fraction=1.0; //fraction of sites to be occupied by dipoles
+int DipoleCutOff=3;
+
+// These variables control the number of loops
+int MCMegaSteps=400;
+int MCEqmSteps=10;
+double MCMegaMultiplier=1.0;
+long long int MCMinorSteps=0;
+char const *LOGFILE = NULL; //for output filenames
+
+// Simulation display / calculation flags
+// False = 0 ; True = 1
+int DisplayDumbTerminal=true;
+int CalculateRecombination=true;
+int CalculateRadialOrderParameter=false;
+      
+int SaveDipolesPNG=false;
+int SaveDipolesSVG=false;
+int SavePotentialXYZ=false;
+
+//END OF SIMULATION PARAMETERS
+// {{ Except for the ones hardcoded into the algorithm :^) }}
+
+void load_config()
+{
+    int i,j,k, x,y; //for loop iterators
+
+    config_t cfg, *cf; //libconfig config structure
+    const config_setting_t *setting;
+    double tmp;
+
+    char name[100];
+    // Yes, I know, 50 chars are enough for any segfault ^_^
+
+    //Load and parse config file
+    cf = &cfg;
+    config_init(cf);
+
+    if (!config_read_file(cf,"starrynight.cfg")) 
+    {
+        fprintf(stderr, "%s:%d - %s\n",
+                config_error_file(cf),
+                config_error_line(cf),
+                config_error_text(cf));
+        config_destroy(cf);
+        exit(EXIT_FAILURE);
+    }
+
+    config_lookup_string(cf,"LOGFILE",&LOGFILE); //library does its own dynamic allocation
+
+    config_lookup_int(cf,"T",&T);
+
+    config_lookup_float(cf,"Efield.x",&tmp);  Efield.x=(float)tmp;
+    config_lookup_float(cf,"Efield.y",&tmp);  Efield.y=(float)tmp;
+    config_lookup_float(cf,"Efield.z",&tmp);  Efield.z=(float)tmp;
+
+    fprintf(stderr,"Efield: x %f y %f z %f\n",Efield.x,Efield.y,Efield.z);
+
+    //    config_lookup_float(cf,"Eangle",&Eangle);
+
+    config_lookup_float(cf,"K",&K);
+    config_lookup_float(cf,"Dipole",&Dipole);
+    config_lookup_float(cf,"CageStrain",&CageStrain);
+
+    /*
+    // read in list of dipoles + prevalence for solid mixture
+    setting = config_lookup(cf, "Dipoles");
+    dipolecount   = config_setting_length(setting);
+    for (i=0;i<dipolecount;i++)
+    dipoles[i].length=config_setting_get_float_elem(setting,i);
+    setting = config_lookup(cf, "Prevalence");
+    dipolecount   = config_setting_length(setting);
+    for (i=0;i<dipolecount;i++)
+    dipoles[i].prevalence=config_setting_get_float_elem(setting,i);
+
+    // stderr printf to check we read correctly
+    for (i=0;i<dipolecount;i++)
+    fprintf(stderr,"Dipole: %d Length: %f Prevalence: %f\n",i,dipoles[i].length, dipoles[i].prevalence);
+    */
+    // above doesn't do anything currently - not sure whether I lost the code
+    // at some point?
+
+    config_lookup_float(cf,"DipoleFraction",&dipole_fraction);
+
+    config_lookup_int(cf,"DipoleCutOff",&DipoleCutOff);
+
+    config_lookup_int(cf,"MCEqmSteps",&MCEqmSteps);
+
+    config_lookup_int(cf,"MCMegaSteps",&MCMegaSteps); 
+    config_lookup_float(cf,"MCMoves",&MCMegaMultiplier);
+
+    MCMinorSteps=(int)((float)X*(float)Y*(float)Z*MCMegaMultiplier);
+
+// Simulation display / calculation flags
+    config_lookup_bool(cf,"DisplayDumbTerminal",&DisplayDumbTerminal);
+    config_lookup_bool(cf,"CalculateRecombination",&CalculateRecombination);
+    config_lookup_bool(cf,"CalculateRadialOrderParameter",&CalculateRadialOrderParameter);
+      
+    config_lookup_bool(cf,"SaveDipolesPNG",&SaveDipolesPNG);
+    config_lookup_bool(cf,"SaveDipolesSVG",&SaveDipolesSVG);
+    config_lookup_bool(cf,"SavePotentialXYZ",&SavePotentialXYZ);
+
+
+
+    fprintf(stderr,"Config loaded. \n");
+}
+
 // 3-Vector dot-product... hand coded, should probably validate against
 // a proper linear albegra library
+// + also check generated machine code that it unrolls nicely + etc.
 static float dot(struct dipole *a, struct dipole *b)
 {
     int D;
@@ -145,76 +240,4 @@ static void random_X_point(struct dipole *p)
     p->z = (float) z;
 }
 
-void load_config()
-{
-    int i,j,k, x,y; //for loop iterators
-
-    config_t cfg, *cf; //libconfig config structure
-    const config_setting_t *setting;
-    double tmp;
-
-    char name[100];
-    // Yes, I know, 50 chars are enough for any segfault ^_^
-
-    //Load and parse config file
-    cf = &cfg;
-    config_init(cf);
-
-    if (!config_read_file(cf,"starrynight.cfg")) 
-    {
-        fprintf(stderr, "%s:%d - %s\n",
-                config_error_file(cf),
-                config_error_line(cf),
-                config_error_text(cf));
-        config_destroy(cf);
-        exit(EXIT_FAILURE);
-    }
-
-    config_lookup_string(cf,"LOGFILE",&LOGFILE); //library does its own dynamic allocation
-
-    config_lookup_int(cf,"T",&T);
-
-    config_lookup_float(cf,"Efield.x",&tmp);  Efield.x=(float)tmp;
-    config_lookup_float(cf,"Efield.y",&tmp);  Efield.y=(float)tmp;
-    config_lookup_float(cf,"Efield.z",&tmp);  Efield.z=(float)tmp;
-
-    fprintf(stderr,"Efield: x %f y %f z %f\n",Efield.x,Efield.y,Efield.z);
-
-    //    config_lookup_float(cf,"Eangle",&Eangle);
-
-    config_lookup_float(cf,"K",&K);
-    config_lookup_float(cf,"Dipole",&Dipole);
-    config_lookup_float(cf,"CageStrain",&CageStrain);
-
-    /*
-    // read in list of dipoles + prevalence for solid mixture
-    setting = config_lookup(cf, "Dipoles");
-    dipolecount   = config_setting_length(setting);
-    for (i=0;i<dipolecount;i++)
-    dipoles[i].length=config_setting_get_float_elem(setting,i);
-    setting = config_lookup(cf, "Prevalence");
-    dipolecount   = config_setting_length(setting);
-    for (i=0;i<dipolecount;i++)
-    dipoles[i].prevalence=config_setting_get_float_elem(setting,i);
-
-    // stderr printf to check we read correctly
-    for (i=0;i<dipolecount;i++)
-    fprintf(stderr,"Dipole: %d Length: %f Prevalence: %f\n",i,dipoles[i].length, dipoles[i].prevalence);
-    */
-    // above doesn't do anything currently - not sure whether I lost the code
-    // at some point?
-
-    config_lookup_float(cf,"DipoleFraction",&dipole_fraction);
-
-    config_lookup_int(cf,"DipoleCutOff",&DipoleCutOff);
-
-    config_lookup_int(cf,"MCEqmSteps",&MCEqmSteps);
-
-    config_lookup_int(cf,"MCMegaSteps",&MCMegaSteps); 
-    config_lookup_float(cf,"MCMoves",&MCMegaMultiplier);
-
-    MCMinorSteps=(int)((float)X*(float)Y*(float)Z*MCMegaMultiplier);
-
-    fprintf(stderr,"Config loaded. \n");
-}
 
--- starrynight-main.c
+++ starrynight-main.c
@@ -78,24 +78,23 @@ int main(int argc, char *argv[])
 
     lattice_Efield_XYZ("initial_lattice_efield.xyz");
 //    lattice_Efieldoffset_XYZ("initial_lattice_efieldoffset.xyz");
-    lattice_potential_XYZ("initial_lattice_potential.xyz"); // potential distro
-    outputlattice_svg("initial-SVG.svg");
+    if(SavePotentialXYZ) lattice_potential_XYZ("initial_lattice_potential.xyz"); // potential distro
+    if(SaveDipolesSVG) outputlattice_svg("initial-SVG.svg");
     outputpotential_png("initial_pot.png"); //"final_pot.png");
     outputlattice_xyz("initial_dipoles.xyz");
  
     fprintf(stderr,"Intial lattice recombination: \n");
     
     fprintf (stderr,"LOCAL ORDER: \n");
-//    radial_order_parameter();
+    if(CalculateRadialOrderParameter) radial_order_parameter();
     // output initialised lattice - mainly for debugging
-/*    outputlattice_ppm_hsv("initial.png");
-    outputlattice_svg("initial-SVG.svg");
-    outputpotential_png("initial_pot.png"); //"final_pot.png");
-    outputlattice_xyz("initial_dipoles.xyz");
-    outputlattice_xyz_overprint("initial_overprint.xyz");
-*/
-    outputlattice_dumb_terminal(); //Party like it's 1980
-//    recombination_calculator(stderr);
+    if(SaveDipolesPNG) outputlattice_ppm_hsv("initial.png");
+    if(SavePotentialXYZ) outputpotential_png("initial_pot.png"); //"final_pot.png");
+    //outputlattice_xyz("initial_dipoles.xyz");
+    //outputlattice_xyz_overprint("initial_overprint.xyz");
+    
+    if (DisplayDumbTerminal) outputlattice_dumb_terminal(); //Party like it's 1980
+    if (CalculateRecombination) recombination_calculator(stderr);
 
     fprintf(stderr,"\n\tMC startup. 'Do I dare disturb the universe?'\n");
 
@@ -114,9 +113,9 @@ int main(int argc, char *argv[])
             MC_move();
     }
  
-    lattice_Efield_XYZ("equilib_lattice_efield.xyz");
-    outputlattice_svg("equilib-SVG.svg");
-    outputpotential_png("equilib_pot.png"); //"final_pot.png");
+    if(SavePotentialXYZ) lattice_Efield_XYZ("equilib_lattice_efield.xyz");
+    if(SaveDipolesSVG)   outputlattice_svg("equilib-SVG.svg");
+    if(SavePotentialXYZ) outputpotential_png("equilib_pot.png"); //"final_pot.png");
  
 //    double AMP; double PHASE;
 //    for (AMP=0.01; AMP<=0.05; AMP+=0.01)
@@ -179,10 +178,9 @@ int main(int argc, char *argv[])
             // Update the (interactive) user what we're up to
             //fprintf(stderr,".");
             //fprintf(stderr,"\n");
-            outputlattice_dumb_terminal(); //Party like it's 1980
-            recombination_calculator(log);
-
-//            radial_order_parameter(); // outputs directly to Terminal
+            if(DisplayDumbTerminal) outputlattice_dumb_terminal(); //Party like it's 1980
+            if(CalculateRecombination) recombination_calculator(log);
+            if(CalculateRadialOrderParameter) radial_order_parameter(); // outputs directly to Terminal
 
             fprintf(stderr,"Efield: x %f y %f z %f | Dipole %f CageStrain %f K %f\n",Efield.x,Efield.y,Efield.z,Dipole,CageStrain,K);
 //            fprintf(stderr,"dipole_fraction: %f T: %d Landau: %f\n",dipole_fraction,T,landau_order());
@@ -197,19 +195,19 @@ fprintf(stderr,"\n");
             sprintf(prefix,"T_%04d_i_%03d_CageStrain_%f",T,i,CageStrain);
 
             sprintf(name,"%s_lattice_efield.xyz",prefix);
-            lattice_Efield_XYZ(name);
+            if(SavePotentialXYZ) lattice_Efield_XYZ(name);
 
             sprintf(name,"%s_lattice_potential.xyz",prefix);
-            lattice_potential_XYZ(name); // potential distro
+            if(SavePotentialXYZ) lattice_potential_XYZ(name); // potential distro
     
             sprintf(name,"%s_MC-PNG_final.png",prefix);
-            outputlattice_ppm_hsv(name);
+            if(SaveDipolesPNG) outputlattice_ppm_hsv(name);
 
             sprintf(name,"%s_MC-SVG_final.svg",prefix);
-            outputlattice_svg(name);
+            if(SaveDipolesSVG) outputlattice_svg(name);
 
             sprintf(name,"%s_potential.png",prefix);
-            outputpotential_png(name); //"final_pot.png");
+            if(SavePotentialXYZ) outputpotential_png(name); //"final_pot.png");
 
             // Manipulate the run conditions depending on simulation time
             //        if (i==100) { DIM=3;}  // ESCAPE FROM FLATLAND
