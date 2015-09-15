8f94e1d180262283567e6749a6a81c19ef930956
Jarvist Moore Frost <jarvist@gmail.com>
2015-09-15 20:19:31 +0100

    Refactor Julia recombination into module + functions. Cleaning up work before expanding remit of module into mobility.

---
+++ recombination/recombination-demo.jl
@@ -0,0 +1,20 @@
+import recombination
+
+T=300           # Temperature; Kelvin
+
+kb=8.6173324E-5 # in units of eV
+β=1/(kb*T)      # Thermodynamic Beta; units
+
+#N=1000
+#pot=0.13*randn(N) # Normal distribution
+#pot=0.13*sin(range(0,N)) # Sinusoidal variations
+
+N,pot = recombination.starrynight_read_potential(ARGS[1])
+
+println("Boltzmann statistics...")
+recombination.calc_recombination(N, pot, En -> exp(-β*En) )
+println("Fermi-Dirac statistics...")
+recombination.calc_recombination(N, pot, En -> 1./(exp(β*En) + 1.0) )
+println("Fermi-Dirac, chem pot = 1.0 eV...")
+recombination.calc_recombination(N, pot, En -> 1./(exp(β*(En+1.0)) + 1.0) )
+
--- recombination/recombination.jl
+++ recombination/recombination.jl
@@ -1,42 +1,47 @@
+module recombination
+
 #Read in potential...
+function starrynight_read_potential(filename)
+    lattice=readdlm(filename)
+    pot=lattice[:,4] # just potential, in internal units of Starrynight
 
-lattice=readdlm(ARGS[1])
-pot=lattice[:,4] # just potential, in internal units of Starrynight
+    T=300           # Temperature; Kelvin
 
-T=300           # Temperature; Kelvin
+    kb=8.6173324E-5 # in units of eV
+    β=1/(kb*T)      # Thermodynamic Beta; units
 
-kb=8.6173324E-5 # in units of eV
-β=1/(kb*T)      # Thermodynamic Beta; units
+    Å=1E-10 # Angstrom
+    r=6.29Å # Sensible figure for MA for consistency; AW
+    D=3.336E-30 #Debye in SI
+    ε0=8.854E-12 #Units: C2N−1m−2
+    D1=2.18D #Methyl-Ammonia; b3lyp/6-31g*
 
-Å=1E-10 # Angstrom
-r=6.29Å # Sensible figure for MA for consistency; AW
-D=3.336E-30 #Debye in SI
-ε0=8.854E-12 #Units: C2N−1m−2
-D1=2.18D #Methyl-Ammonia; b3lyp/6-31g*
+    # OK; let's calc Dipole Potential with simple form, contribution from one dipole at distance 1
+    factor=1/(4*pi*ε0) * (D1*r)/(r^3)
+    @printf "Conversion factor from Starrynight potential internal units --> V : %f\n" factor
+    # Scale from Starrynight potential --> volts
+    pot*=factor
 
-# OK; let's calc Dipole Potential with simple form, contribution from one dipole at distance 1
-factor=1/(4*pi*ε0) * (D1*r)/(r^3)
-@printf "Conversion factor from Starrynight potential internal units --> V : %f\n" factor
-# Scale from Starrynight potential --> volts
-pot*=factor
+    # Apply effective dielectric screening...
+    # Calculated value DFT: 5.6 --> 6.5 (dep. on MA alignment)
+    # See: APL Mat. 1, 042111 (2013); http://dx.doi.org/10.1063/1.4824147
+    # Measured value, Ellipso in NIR: 5.0
+    ɛr=5.0
+    pot/=ɛr
 
-# Apply effective dielectric screening...
-# Calculated value DFT: 5.6 --> 6.5 (dep. on MA alignment)
-# See: APL Mat. 1, 042111 (2013); http://dx.doi.org/10.1063/1.4824147
-# Measured value, Ellipso in NIR: 5.0
-ɛr=5.0
-pot/=ɛr
+    N=length(pot) # number of elements
 
-N=length(pot) # number of elements
+    @printf "OK; data loaded and converted to Volts. Variance: %e SD: %e\n\n" var(pot) std(pot)
+    print(hist(pot,50))
 
-@printf "OK; data loaded and converted to Volts. Variance: %e SD: %e\n\n" var(pot) std(pot)
-print(hist(pot,50))
+    return N, pot
+end
 
-N=1000
+#N=1000
 #pot=0.13*randn(N) # Normal distribution
 #pot=0.13*sin(range(0,N)) # Sinusoidal variations
 
-function recombination(kernel)
+function calc_recombination(N,pot,kernel)
     # Calculate partition function by sum over state energies
     Zh=sum(kernel(-pot))
     Ze=sum(kernel(pot))
@@ -53,10 +58,4 @@ function recombination(kernel)
     println()
 end
 
-println("Boltzmann statistics...")
-recombination( En -> exp(-β*En) )
-println("Fermi-Dirac statistics...")
-recombination( En -> 1./(exp(β*En) + 1.0) )
-println("Fermi-Dirac, chem pot = 1.0 eV...")
-recombination( En -> 1./(exp(β*(En+1.0)) + 1.0) )
-
+end 
