4900c347465d3c2d9ba996962896d9a02a55e1b8
Jarvist Moore Frost <jarvist@gmail.com>
2018-02-01 15:31:07 +0000

    Use a cross-simulation volume Gaussian; fixes discretisation errors.
    Actually caused much worse errors at first! As my cutoff was far too
    small, leading to the recombination tending to a finite value quite
    significantly less than 1.0.
    Now with the full simulation volume, the computational cost of
    processing the data is quite high. (But you only do this once!)

--- recombination/recombination-demo.jl
+++ recombination/recombination-demo.jl
@@ -17,8 +17,8 @@ function potential_statistics(N,pot)
     recombination.calc_recombination(N, pot, En -> exp.(-β*En) )
     println("Fermi-Dirac statistics...")
     recombination.calc_recombination(N, pot, En -> 1./(exp.(β*En) + 1.0) )
-    println("Fermi-Dirac, chem pot = 1.0 eV...")
-    recombination.calc_recombination(N, pot, En -> 1./(exp.(β*(En+1.0)) + 1.0) )
+    println("Fermi-Dirac, chem pot = 0.8 eV...")
+    recombination.calc_recombination(N, pot, En -> 1./(exp.(β*(En+0.8)) + 1.0) )
     recombination.calc_mobility(N,pot)
 end
 
@@ -27,7 +27,7 @@ potential_statistics(N,pot)
 # Apply knowledge of polaron Gaussian wavefunction by Gaussian blurring the underlying potential
 
 using Images
-n=100 # Terrible hard-wired hack
+n=50# Terrible hard-wired hack
 pot=reshape(pot,n,n,n)
 #print(pot)
 
@@ -37,9 +37,20 @@ pot=reshape(pot,n,n,n)
 #pot[n/2,n/2,n/2]=1.0
 
 
+const l=49 # size of Gaussian Kernel; make this larger to avoid edge effects
+# defaults to: l = 4*ceil(Int,σ)+1) ; which is why it had a discretisation error
+# c.f.
+# https://github.com/JuliaImages/ImageFiltering.jl/blob/f9195cc14f24dc5bcb3d45adb1608a7d7d103018/src/kernelfactors.jl#L267
+
 #print(pot)
 for i=0:0.05:10
     println(STDERR,"Gaussian filter with Sigma=$i")
-    R=recombination.calc_recombination(N,imfilter(pot,Kernel.gaussian([i,i,i])), En -> 1./(exp.(β*(En+1.0)) + 1.0) )
+    R=recombination.calc_recombination(N,
+                                       imfilter(pot,reflect(Kernel.gaussian([i,i,i], [l,l,l])), "circular"), 
+                                       En -> 1./(exp.(β*(En+0.8)) + 1.0) )
+    # circular gives periodic boundaries - actually not sure of this, may have broken.
+    # reflect() makes it operation a convolution
+    # (i,i,i) define the standard deviation of the Gaussian
+    # (l,l,l) are the size of the Kernel (stencil); set larger than default to avoid discretisation error
     println("$i $R") # STDOUT, for later printing.
 end
